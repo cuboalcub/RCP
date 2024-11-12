4302a2a63858de4a7731fa00595f1f076d2eda33
cuboalcub <zs22019951@estudiantes.uv.mx>
2024-11-12 17:18:00 -0600

    Creacion de servicio de registro conexion con el API comunicacion con el API exitosa

---
+++ src/API-Routes.ts
@@ -0,0 +1,7 @@
+export const API_ROUTES = {
+    baseUrl: 'http://127.0.0.1:8000',
+    user:{
+        login:'/user/login',
+        signup:'/user/signup',
+    }
+}
--- src/sign-up/sign-up.component.ts
+++ src/sign-up/sign-up.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { SignupService } from '../services/signup.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './sign-up.component.html',  // Asegúrate de que este archivo exista
-     // Asegúrate de que este archivo exista
 })
-export class SignUpComponent { }
+export class SignUpComponent { 
+  username: string = "";
+  password: string = "";
+  confirmPassword: string = "";
+  email: string = "";
+
+  constructor(private signupService: SignupService, private router: Router) { }
+
+  signup() {
+    this.signupService.signup(this.username, this.password, this.email).subscribe(
+      () => {
+        alert("Cuenta creada con éxito");
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        alert("Error al crear la cuenta");
+        console.log(error);
+      }
+    );
+  }
+}
