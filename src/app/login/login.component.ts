4455a35981953f65dcf6517ae02c044a1e94e639
cuboalcub <zs22019951@estudiantes.uv.mx>
2024-11-12 17:41:25 -0600

    rebase de la rama login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+  username: string = "";
+  password: string = "";
+  constructor(private router: Router) { }
+
+  goBack() {
+    this.router.navigate(['/']);
+  }
+
+  goLogin() {
+    
+  }
+
+  goSignUp() {
+    this.router.navigate(['/signup']);
+  }
 
 }
