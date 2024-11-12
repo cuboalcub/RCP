180b34d6754f9d51d532dfeb07c5288be6633355
cuboalcub <zs22019951@estudiantes.uv.mx>
2024-11-12 09:42:51 -0600

    update media url

--- src/landing/landing.component.ts
+++ src/landing/landing.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MediaRCP } from '../MediaForRCP/MediaRCP';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrls: ['./landing.component.css'] // Corregido a 'styleUrls'
 })
 export class LandingComponent {
+  Media: typeof MediaRCP = MediaRCP; // Definición de tipo correcta para la propiedad 'Media'
 
+  ngOnInit(): void {
+    // Código para ejecutar al inicializar el componente
+  }
 }
