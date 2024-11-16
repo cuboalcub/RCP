1af8296c197e28304c461647933af5fa9722a462
cuboalcub <zs22019951@estudiantes.uv.mx>
2024-11-16 08:16:14 -0600

    acomodo de carpetas

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Project } from '../models/project/project.modelo';
-import { HomeService } from '../services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +8,46 @@ import { HomeService } from '../services/home.service';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  menuVisible = false;
+  datosTabla = [
+    {
+      nombre: "Nombre 1",
+      optimo: "Valor 1",
+      probable: "Valor 2",
+      pesimo: "Valor 3"
+    },
+    {
+      nombre: "Nombre 2",
+      optimo: "Valor 4",
+      probable: "Valor 5",
+      pesimo: "Valor 6"
+    }
+  ];
+  
+
+  toggleMenu(event: MouseEvent): void {
+    event.stopPropagation();
+    this.menuVisible = !this.menuVisible;
+  }
+
+  @HostListener('document:click')
+  closeMenu(): void {
+    this.menuVisible = false;
+  }
+
+  logOut(): void {
+    localStorage.removeItem('userSession');
+    alert('Has cerrado sesión.');
+    window.location.href = '/'; // Redirigir al inicio tras cerrar sesión
+  }
+
+
+  eliminarElemento(elemento: any) {
+    const index = this.datosTabla.indexOf(elemento);
+    if (index > -1) {
+      this.datosTabla.splice(index, 1);
+      console.log('Elemento eliminado:', elemento);
+    }
+  }
 
 }
